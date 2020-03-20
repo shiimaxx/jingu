489b969ff121989f3a2fedb69fcd53c3473a47fd
shiimaxx <yoshima@shiimaxx.com>
2020-03-20 11:26:58 +0900

    Use auto() in Enum

--- jingu/lexer.py
+++ jingu/lexer.py
@@ -1,23 +1,23 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class TokenType(Enum):
-    DATA = 1
-    NAME = 2
-    VARIABLE_BEGIN = 3
-    VARIABLE_END = 4
-    BLOCK_BEGIN = 15
-    BLOCK_END = 16
-    LBRACKET = 5
-    RBRACKET = 6
-    INTEGER = 7
-    STRING = 8
-    DOT = 9
-    ADD = 10
-    SUB = 11
-    MUL = 12
-    DIV = 13
-    MOD = 14
+    DATA = auto()
+    NAME = auto()
+    VARIABLE_BEGIN = auto()
+    VARIABLE_END = auto()
+    BLOCK_BEGIN = auto()
+    BLOCK_END = auto()
+    LBRACKET = auto()
+    RBRACKET = auto()
+    INTEGER = auto()
+    STRING = auto()
+    DOT = auto()
+    ADD = auto()
+    SUB = auto()
+    MUL = auto()
+    DIV = auto()
+    MOD = auto()
 
 
 class Token(object):
